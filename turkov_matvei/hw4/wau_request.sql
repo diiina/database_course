bef4bf33990e06b3720f4888f9ecfd8885db33bb
turk0v <turkoov@gmail.com>
2019-03-12 15:30:21 +0300

    wau request and result

---
+++ turkov_matvei/hw4/wau_request.sql
@@ -0,0 +1,12 @@
+with recursive cte as (
+  select min(cast(begin_dttm as date )) as day_of_week from sessions
+  union all
+  select day_of_week + interval 1 week from cte
+  where day_of_week + interval 1 week <= (select max(cast(begin_dttm as date)) from sessions)
+)
+select cte.day_of_week,count(distinct sessions.user_id)
+from cte,sessions
+where cte.day_of_week <= cast(sessions.begin_dttm as date)
+and cte.day_of_week + interval 1 week > cast(sessions.begin_dttm as date)
+group by  cte.day_of_week
+order by cte.day_of_week
