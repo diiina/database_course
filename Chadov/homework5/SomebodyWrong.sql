d6f9f45d529e035f9b44797d7f773b546419bea2
barrens <barrens@okbsapr.ru>
2019-03-23 22:55:19 +0300

    hw5_bonus

--- Chadov/homework5/SomebodyWrong.sql
+++ Chadov/homework5/SomebodyWrong.sql
@@ -2,21 +2,35 @@
 
 CREATE TABLE Users (
  Id SERIAL PRIMARY KEY,
- Name VARCHAR(50) UNIQUE NOT NULL,
- Email VARCHAR(50) UNIQUE NOT NULL,
+ Name VARCHAR(50) UNIQUE,
+ Email VARCHAR(50) UNIQUE,
  WalletScore BIGINT,
  Score BIGINT,
- sex enum('man', 'woman') NULL, 
- date_birth TIMESTAMP NOT NULL,
- reg_dttm TIMESTAMP NOT NULL
+ Sex enum('man', 'woman'), 
+ DateBirth TIMESTAMP,
+ RegDttm TIMESTAMP
 );
 CREATE INDEX indexName ON Users(Name);
 CREATE INDEX indexEmail ON Users(email);
 
+CREATE TABLE IF NOT EXISTS sessions (
+	Id INT,
+	UserId INT REFERENCES users(Id),
+	BeginDttm TIMESTAMP,
+	EndDttm TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS payments (
+	Id INT,
+	UserId INT REFERENCES users(Id),
+	PaymentSum DOUBLE,
+	PaymentDttm TIMESTAMP
+);
+
 CREATE TABLE Topic (
  Id SERIAL PRIMARY KEY,
- Name VARCHAR(50) UNIQUE NOT NULL,
- Description VARCHAR(1000) NOT NULL,
+ Name VARCHAR(50) UNIQUE,
+ Description VARCHAR(1000),
  DialogsCount BIGINT
 );
 CREATE INDEX indexName ON Topic(Name);
