cd8126935215cc73d1ba482824bcf361828150ed
alekseik1 <1alekseik1@gmail.com>
2019-04-01 12:44:08 +0300

    dau and explain results (no optimization)

---
+++ Kozharin Aleksei/homework7/dau.sql
@@ -0,0 +1,13 @@
+# DAU
+EXPLAIN WITH RECURSIVE cte AS
+(
+    SELECT MIN(CAST(begin_dttm AS DATE)) AS dt FROM sessions
+        UNION ALL
+	SELECT dt + INTERVAL 1 DAY
+      FROM cte
+     WHERE dt + INTERVAL 1 DAY <= (SELECT MAX(CAST(begin_dttm AS DATE)) FROM sessions)
+)
+SELECT cte.dt AS 'date', COUNT(DISTINCT sessions.user_id) AS 'DAU'
+  FROM sessions RIGHT JOIN cte ON CAST(sessions.begin_dttm AS DATE) = cte.dt
+ GROUP BY cte.dt
+ ORDER BY cte.dt
