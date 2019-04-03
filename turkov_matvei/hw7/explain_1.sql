7e6bc472ada2561ba8f2875d749115f547405037
turk0v <turkoov@gmail.com>
2019-04-03 23:37:57 +0300

    added explain and useful index

---
+++ turkov_matvei/hw7/explain_1.sql
@@ -0,0 +1,12 @@
+EXPLAIN with recursive cte as (
+  select min(cast(view_time as date )) as day_t from Views
+  union all
+  select day_t + interval 1 week from cte
+  where day_t + interval 1 week <= (select max(cast(view_time as date)) from Views)
+)
+select cte.day_t,count(distinct Views.user_id)
+from cte,Views
+where cte.day_t <= cast(views.view_time as date)
+and cte.day_t + interval 1 day > cast(Views.view_time as date)
+group by  cte.day_t
+order by cte.day_t
